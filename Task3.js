429cb06750e012ed8a42bbdd8beacc5557d9621f
Oluseyi45 <62138621+Oluseyi45@users.noreply.github.com>
2020-04-19 21:19:33 +0100

    Add files via upload

---
+++ Task3.js
@@ -0,0 +1,33 @@
+
+<script>
+let userInput = prompt("Enter any number");
+let userNo= parselnt(userInput);
+let check = [];
+for (let i=1;i<(user); i++){
+if (i==2){
+let a = "yu"; //replacing  2 by "yu"
+check.push(a);
+} else if(i==3){
+let a="gi";  //replacing 3 by "gi"
+check.push(a);
+} else if(i==5){
+ let a = "oh";  //replacing 5 by "oh"
+check.push(a);
+} else if (i%==0 && i%==0 &&5==0);{
+ let a ="yu-gi-oh"; //
+ check.push(a);
+} else if(i%2==0 &&i%5==0);{
+ let a ="yu-oh";  
+ check.push(a);
+} else if(i%2==0 && i%3==0);{
+ let a = "yu-gi";
+ check.push(a);
+} else if (i%3==0 && i%5==0);{
+ let a = "gi-oh";
+check.push(a);
+} else {check.push(i);
+}
+}
+console.log(check);
+
+<script>
